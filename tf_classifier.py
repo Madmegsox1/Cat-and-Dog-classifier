4291284cea01f3156d2f30cbd80acf4b510048e7
Madmegsox1 <68086870+Madmegsox1@users.noreply.github.com>
2021-01-12 15:40:42 +0000

    added more labels

--- tf_classifier.py
+++ tf_classifier.py
@@ -3,7 +3,7 @@
 """
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # stops tf beeing annoying 
 
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -14,8 +14,8 @@ from PIL import Image
 
 train = ImageDataGenerator(rescale=1 / 255)
 valid = ImageDataGenerator(rescale=1 / 255)
-class_names = ['cat', 'dog']
-
+class_names = ['cat', 'dog']  # sets class names
+ # ⬇️ this creats the data sets for the training and validation
 """
 train_ds = train.flow_from_directory("dog_cat\\training_set\\training_set\\",
                                      target_size=(200, 200),
@@ -28,6 +28,7 @@ valid_ds = train.flow_from_directory("dog_cat\\test_set\\test_set\\",
                                      class_mode='binary')
 """
 # just uncomment this if you want to use it
+# ⬇️ this is the model i used for the classification
 """ 
 model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(16, (3, 3), activation="relu", input_shape=(200, 200, 3)),
@@ -42,12 +43,12 @@ model = tf.keras.Sequential([
 ])
 """
 
-model = tf.keras.models.load_model('dog_cat/saved_model/cat_and_dog_model')
+model = tf.keras.models.load_model('dog_cat/saved_model/cat_and_dog_model')  # loads the saved model 
 
-model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),  # compiles the model
               optimizer="adam",
               metrics=['accuracy'])
-"""
+"""  # trains the model, but isnt needed as the model is saved
 epochs = 10 q
 history = model.fit(
     train_ds,
@@ -55,22 +56,22 @@ history = model.fit(
     epochs=epochs
 )
 """
-model.save('dog_cat/saved_model/cat_and_dog_model')
+model.save('dog_cat/saved_model/cat_and_dog_model') # saves the model
 
 dir_path = "dog_cat/working_set"
 
-for f in os.listdir(dir_path):
-    img = image.load_img(dir_path + "//" + f, target_size=(200, 200))
+for f in os.listdir(dir_path):  # loops throge all the images in "dog_cat/working_set"
+    img = image.load_img(dir_path + "//" + f, target_size=(200, 200))  # resizes the images to the 200x200 format it was trained with
 
     # Image.open(dir_path+"//"+f).show()
 
-    x = image.img_to_array(img)
+    x = image.img_to_array(img)  # coverts the images to an array
     x = np.expand_dims(x, axis=0)
-    images = np.vstack([x])
+    images = np.vstack([x])  # does math
 
-    pre = model(images, training=False)
+    pre = model(images, training=False)  # classifies the images
     for i, logits in enumerate(pre):
-        class_id = tf.argmax(logits).numpy()  # converts its output
+        class_id = tf.argmax(logits).numpy()  # converts it to a readble output
         p = tf.nn.softmax(logits)[class_id]
         name = class_names[class_id]
-        print("{} is a {} ({:4.1f}%)".format(f, name, 100 * p))
+        print("{} is a {} ({:4.1f}%)".format(f, name, 100 * p))  # prints the output 
